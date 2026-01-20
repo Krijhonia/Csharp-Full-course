dc0c03090ee63f0b648ec3a486e5b98b373278bf
Kunal Rijhonia <krijhonia@gmail.com>
2026-01-20 13:22:31 +0530

    Added const keyword and storing user data in the course

---
+++ ConsoleApp1/ConsoleApp1/Const keyword/index.cs
@@ -0,0 +1,7 @@
+// const keyword in C#       
+const int vat = 20;
+const double percentage = vat/100D;
+
+int balance = 1000;
+
+System.Console.WriteLine(balance * percentage);
--- ConsoleApp1/ConsoleApp1/Var keyword/index.cs
+++ ConsoleApp1/ConsoleApp1/Var keyword/index.cs
@@ -1,2 +1,9 @@
-// Var keyword in C#
-var age = 23;
+// Var in C#  
+
+// Explicitly typed
+int age = 25;
+string name = "Gemini";
+
+// Implicitly typed (same result)
+var age = 25;      // Compiled as int
+var name = "Gemini"; // Compiled as string
