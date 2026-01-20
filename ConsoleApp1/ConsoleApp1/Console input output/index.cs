796578e7219f9aa235d2bef392a069297d477937
Kunal Rijhonia <krijhonia@gmail.com>
2026-01-20 13:46:21 +0530

    Added console input and output in the course

---
+++ ConsoleApp1/ConsoleApp1/Console input output/index.cs
@@ -0,0 +1,13 @@
+// Console input / output in Csharp
+
+
+
+Console.WriteLine("Hello my name is Kunal");
+Console.Write("What is your name: ");
+
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.Write("What is your age: ");
+
+string age = Console.ReadLine();
+Console.WriteLine(age);
---
+++ ConsoleApp1/ConsoleApp1/Odd Even number checker/index.cs
@@ -0,0 +1,9 @@
+// Checking odd and even 
+int numOne = 10;
+int numTwo = 2;
+int remainder = numOne % numTwo;
+System.Console.WriteLine(remainder);
+
+numOne = 11;
+remainder = numOne % numTwo;
+System.Console.WriteLine(remainder);
