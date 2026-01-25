2e3a3b18ed254459661513e091bcedc271222517
Kunal Rijhonia <krijhonia@gmail.com>
2026-01-25 13:28:01 +0530

    Everything is upto date

--- ConsoleApp1/ConsoleApp1/If statements/index.cs
+++ ConsoleApp1/ConsoleApp1/If statements/index.cs
@@ -4,11 +4,23 @@ System.Console.WriteLine("What is your age: ");
 string ageInput = Console.ReadLine();
 int age = Convert.ToInt32(ageInput);
 
-if (age >= 18)
-{
-    System.Console.WriteLine("You are an adult");
-}
-else if(age<0||age>150)
+
+if(age<0||age>150)
 {
     System.Console.WriteLine("Invalid age");
+}
+else
+{
+    if (age >= 18 && age <= 25)
+    {
+    System.Console.WriteLine("Your age is between 18 and 25");
+    }
+    else if(age >= 26)
+    {
+     System.Console.WriteLine("Your age is 26 or greater than that");   
+    }
+    else if(age<0 || age > 150)
+    {
+        System.Console.WriteLine("Invalid age!");
+    }    
 }
